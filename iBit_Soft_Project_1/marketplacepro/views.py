b71c80b5b47c9beaf2409d7933d76ea0ca8ef71f
wazaaah <mcnobert1amoah@gmail.com>
2024-09-02 16:53:51 +0000

    Numero Two

--- iBit_Soft_Project_1/marketplacepro/views.py
+++ iBit_Soft_Project_1/marketplacepro/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def thankyou(request):
+    return render(request, 'thankyou.html')
