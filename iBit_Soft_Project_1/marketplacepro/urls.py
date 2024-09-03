967af2d9bc82be3321f804cdcd39eec9334e21de
wazaaah <mcnobert1amoah@gmail.com>
2024-09-03 10:13:17 +0000

    Numero Three

---
+++ iBit_Soft_Project_1/marketplacepro/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('shop', views.shop, name='shop'),
+    path('contact', views.contact, name='contact'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
+    path('thankyou', views.thankyou, name='thankyou'),
+
+]
