ea775b367fc65e1fb3bd4d2aff7f8379e3a4a930
wazaaah <mcnobert1amoah@gmail.com>
2024-09-04 11:36:25 +0000

    Numero Nine

--- iBit_Soft_Project_1/marketplacepro/models.py
+++ iBit_Soft_Project_1/marketplacepro/models.py
@@ -57,6 +57,7 @@ class CheckoutItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
